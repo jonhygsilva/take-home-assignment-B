ef4ecb28c718beae87a2f9ea758208bf387369dc
Jonhy Silva <jonhysilva@MacBook-Pro-de-Jonhy.local>
2025-03-15 14:40:14 -0300

    first commit with some initial endpoints

---
+++ src/routes/form.ts
@@ -0,0 +1,79 @@
+import { FastifyInstance } from 'fastify'
+
+import { Form } from '@prisma/client'
+
+import prisma from '../db/db_client'
+import { serializer } from './middleware/pre_serializer'
+import { IEntityId } from './schemas/common'
+import { ApiError } from '../errors'
+
+async function formRoutes(app: FastifyInstance) {
+  app.setReplySerializer(serializer)
+
+  const log = app.log.child({ component: 'formRoutes' })
+
+  app.get<{
+    Params: IEntityId
+    Reply: Form
+  }>('/:id', {
+    async handler(req, reply) {
+      const { params } = req
+      const { id } = params
+      log.debug('get form by id')
+      try {
+        const form = await prisma.form.findUniqueOrThrow({ where: { id } })
+        reply.send(form)
+      } catch (err: any) {
+        log.error({ err }, err.message)
+        throw new ApiError('failed to fetch form', 400)
+      }
+    },
+  })
+
+  app.get<{
+    Params: IEntityId
+    Reply: Form[]
+  }>('', {
+    async handler(req, reply) {
+      log.debug('get all forms')
+
+      try {
+        const form = await prisma.form.findMany()
+        reply.send(form)
+      } catch (err: any) {
+        log.error({ err }, err.message)
+        throw new ApiError('failed to fetch all forms', 400)
+      }
+    },
+  })
+
+  app.post<{
+    Body: {
+      name: string,
+      fields: any
+    },
+    Reply: Form
+  }>('', {
+    async handler(req, reply) {
+      const { name, fields } = req.body
+      log.debug('create a new form')
+
+      try {
+        const form = await prisma.form.create({ 
+            data: {
+              name: name,
+              fields: fields
+            } 
+          }
+        )
+
+        reply.status(201).send(form)
+      } catch (err: any) {
+        log.error({ err }, err.message)
+        throw new ApiError('failed to create form', 400)
+      }
+    },
+  })
+}
+
+export default formRoutes
